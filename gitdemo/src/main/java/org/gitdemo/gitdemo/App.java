49664debf5e00d456118127062b2b7b0889ae450
Adar Margalit <adar.margalit@servicenow.com>
2015-07-07 12:06:22 +0300

    Change from A

--- gitdemo/src/main/java/org/gitdemo/gitdemo/App.java
+++ gitdemo/src/main/java/org/gitdemo/gitdemo/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		// this is a comment
+		System.out.print("bye");
+		// bllaaa
+		System.out.print("bye");
+
     }
 }
